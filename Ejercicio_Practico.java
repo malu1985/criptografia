b0b12a7be81ad6bac91fbb221e36dcac75ff791f
Mariluz Loaiza Sanchez <mloaiza@itcol.com>
2023-07-25 08:44:20 -0500

    codigo critografia

---
+++ Ejercicio_Practico.java
@@ -0,0 +1,81 @@
+package com.mycompany.ejercicio_practico;
+
+import java.util.Scanner;
+
+
+public class Ejercicio_Practico {
+
+    public static void main(String[] args) {
+        Scanner sn = new Scanner (System.in);
+        sn.useDelimiter("\n");
+        String letras="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        
+                
+        System.out.println("INGRESE UNA PALARA");
+        String frase = sn.next();
+        
+        String texto = codificar(letras, frase);
+        System.out.println("Texto codifcado: " + texto);
+        
+        texto = descodificar(letras, texto);
+        System.out.println("Texto Descodifcado: " + texto);
+     }
+    
+    public static String codificar(String letras, String texto){
+     String textoCodificado="";
+     
+     texto = texto.toUpperCase();
+     char caracter;
+     for (int i = 0; i<texto.length(); i++){
+         caracter=texto.charAt(i);
+         
+         int pos = letras.indexOf(caracter);
+         
+         if(pos== -1){
+         
+          textoCodificado += caracter;
+         
+         }else{
+             
+             textoCodificado+= letras.charAt((pos + 4) % letras.length());
+         
+              }
+        }
+        return textoCodificado;
+    
+        }
+   public static String descodificar(String letras, String texto){
+    String textoDescodificado = "";
+    
+    texto=texto.toUpperCase();
+    
+    char caracter;
+    for (int i = 0; i<texto.length(); i++) {
+        caracter = texto.charAt (i);
+        
+        int pos = letras.indexOf(caracter);
+        
+        if (pos== -1){
+            textoDescodificado += caracter;
+                
+        }else {
+            if(pos - 3 < 0){
+                textoDescodificado += letras.charAt(letras.length() + (pos - 4));
+             }else {
+        
+           textoDescodificado+= letras.charAt((pos - 4)% letras.length ());
+        
+            }
+        }
+    
+    
+    }
+        return textoDescodificado;
+    
+        } 
+
+   
+}
+
+   
+
